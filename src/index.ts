0d4fac1470d27886e066c01ecf62a82139a66351
Ashish Bhusal <ashish.bhusal.16999@gmail.com>
2025-05-05 02:57:42 -0700

    chore: Fix bug and remove local dynamo db

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,7 @@ if(!isProduction){
 
 export const clerkClient = createClerkClient({
     secretKey: process.env.CLERK_SECRET_KEY,
+    publishableKey: process.env.CLERK_PUBLISHABLE_KEY,
 });
 
 const app = express();
